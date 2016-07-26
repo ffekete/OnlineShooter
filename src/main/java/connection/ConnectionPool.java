d8a5265959be24a8d96890d28d7543e256165e52
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-07-26 14:39:16 +0200

    Added comments. Removed unnecessary id from ConnectionPool class.

--- src/main/java/connection/ConnectionPool.java
+++ src/main/java/connection/ConnectionPool.java
@@ -9,23 +9,25 @@ import org.springframework.stereotype.Component;
 import config.BrokerPaths;
 import config.ConnectionPreferences;
 
+/** This component stores all connection nodes that are paired to the active players.
+ *  Connection nodes are stored in a map and each player ids have exactly one connection node. 
+ *  */
 @Component
 public class ConnectionPool {
-	
-	private static Long id = 0L;
-	
 	/** A new connection will be paired to each player id */
 	private Map<Long, ConnectionNode> connectionPool;
 	
+	/** Removes a connection node that belongs to the given player id. */
 	public void removeConnectionNode(Long playerId){
 		System.out.println("Connection node removed with id " + connectionPool.get(playerId).getId());
 		connectionPool.remove(playerId);
 	}
 	
-	/** function tries to register a new connection. If the pool is full, it returns false, if not, it stores a new connection node with id and path in the pool. */
+	/** function tries to register a new connection. If the pool is full, it returns null, if not, it stores a new connection node with id and path in the pool and returns with the reference to the connectionId. */
 	public Long registerNewConnection(Long playerId){
 		Long connectionId = findAvailableConnectionNodeId();
 		
+		/* If there is a free connection slot, register a new connection and store it in the pool */
 		if(connectionId != null){
 			ConnectionNode connectionNode = new ConnectionNode(BrokerPaths.PROVIDE_PLAYER_DATA, connectionId);
 			connectionPool.put(playerId, connectionNode);
@@ -34,6 +36,7 @@ public class ConnectionPool {
 		}
 		else
 		{
+			/* There is no free connection slot... */
 			return null;
 		}
 	}
@@ -48,13 +51,12 @@ public class ConnectionPool {
 		}
 		else
 		{
-			id++;
 			/* Get all available connections by removing all used from the list */
 			ArrayList<Long> availableConnections = new ArrayList<Long>();
 			for(long i = 0; i < ConnectionPreferences.SERVER_MAX_CAPACITY; i++){
 				availableConnections.add(i);
 			}
-			
+			/* Remove used connection ids */
 			for(Long id: connectionPool.keySet()){
 				ConnectionNode node = connectionPool.get(id);
 				availableConnections.remove(node.getId());
