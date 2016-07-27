317a803c78b39de95a8f88663a15c96372308069
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-07-27 11:02:13 +0200

    Tuned ship speeds.

--- src/main/java/config/GameConfig.java
+++ src/main/java/config/GameConfig.java
@@ -2,6 +2,6 @@ package config;
 
 public class GameConfig {
 	public final static double BULLET_INITIAL_SPEED = 10.0d;
-	public final static double SHIP_INITIAL_SPEED = 1.3d;
+	public final static double SHIP_INITIAL_SPEED = 2.0d;
 	public final static double BULLET_MAX_AGE = 100d;
 }
