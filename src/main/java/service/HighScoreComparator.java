9183a4c2efb929bb09dbb2f809e93742ed78520f
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-07-29 13:12:13 +0200

    Corrected highScoreComparator

--- src/main/java/service/HighScoreComparator.java
+++ src/main/java/service/HighScoreComparator.java
@@ -7,7 +7,7 @@ public class HighScoreComparator implements Comparator<HighScore>{
 
 	@Override
 	public int compare(HighScore o1, HighScore o2) {
-		if(o1.getScore() < o2.getScore())
+		if(o1.getScore() > o2.getScore())
 			return -1;
 		else if(o1.getScore() < o2.getScore())
 		{
