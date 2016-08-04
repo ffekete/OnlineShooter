ed689a953ec0978b5697f3db7e2ef80183d12bcb
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-08-04 13:12:11 +0200

    Adjusted ship speeds.

--- src/main/java/config/ShipConfig.java
+++ src/main/java/config/ShipConfig.java
@@ -2,18 +2,18 @@ package config;
 
 public class ShipConfig {
 	public final static long   QUICKSILVER_INIT_HP = 15L;
-	public final static double QUICKSILVER_INIT_SPEED = 7.0D;
+	public final static double QUICKSILVER_INIT_SPEED = 6.0D;
 	public final static double QUICKSILVER_INIT_MANEUVERABILITY = 10.0D;
 	
 	public final static long   MERCURY_INIT_HP = 25L;
-	public final static double MERCURY_INIT_SPEED = 6.0D;
+	public final static double MERCURY_INIT_SPEED = 5.0D;
 	public final static double MERCURY_INIT_MANEUVERABILITY = 12.0D;
 	
 	public final static long   INTERCEPTOR_INIT_HP = 20L;
-	public final static double INTERCEPTOR_INIT_SPEED = 8.0D;
+	public final static double INTERCEPTOR_INIT_SPEED = 7.0D;
 	public final static double INTERCEPTOR_INIT_MANEUVERABILITY = 14.0D;
 	
 	public final static long   DELTAWING_INIT_HP = 11L;
-	public final static double DELTAWING_INIT_SPEED = 10.0D;
+	public final static double DELTAWING_INIT_SPEED = 9.0D;
 	public final static double DELTAWING_INIT_MANEUVERABILITY = 16.0D;
 }
