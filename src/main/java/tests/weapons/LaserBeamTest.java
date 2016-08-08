25de8609bafb389dd313f4c7d9d5227c6ac8ba8f
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-08-08 09:50:49 +0200

    Added new testcases for laserBeamTests.

--- src/main/java/tests/weapons/LaserBeamTest.java
+++ src/main/java/tests/weapons/LaserBeamTest.java
@@ -1,47 +1,49 @@
 package tests.weapons;
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import bullet.LaserBeam;
 import model.Coordinate;
 
 public class LaserBeamTest{
+
+	@DataProvider(name = "angleSourceForY")
+	public Object[][] angleSourceForY(){
+		return new Object[][]{
+			{0.0D, 0.0D, 90.0D, 150.0D},
+			{0.0D, 0.0D, 180.0D, 0.0D},
+			{0.0D, 0.0D, 270.0D, -150.0D},
+			{0.0D, 0.0D, 0.0D, 0.0D}
+		};
+	}
 	
-	@Test
-	public void testLaserBeam_shouldCreateLaserBeam_90Angle_XEqualsTo_0(){
-		LaserBeam laserBeam = new LaserBeam(0.0D, 0.0D, 90.0D, 0L, 5L);
+	@DataProvider(name = "angleSourceForX")
+	public Object[][] angleSourceForX(){
+		return new Object[][]{
+			{0.0D, 0.0D, 90.0D, 0.0D},
+			{0.0D, 0.0D, 180.0D, -150.0D},
+			{0.0D, 0.0D, 270.0D, 0.0D},
+			{0.0D, 0.0D, 0.0D, 150.0D}
+		};
+	}
+	
+	@Test(dataProvider = "angleSourceForX")
+	public void testLaserBeam_shouldCreateLaserBeam_XCoordinate(double x, double y, double angle, double expectedXCoordinate){
+		LaserBeam laserBeam = new LaserBeam(x, y, angle, 0L, 5L);
 		
 		Coordinate endPoint = laserBeam.getEndPoint();
 		
-		Assert.assertEquals(endPoint.getX(), 0.0D, 0.00001D);
+		Assert.assertEquals(endPoint.getX(), expectedXCoordinate, 0.00001D);
 	}
 	
-	@Test
-	public void testLaserBeam_shouldCreateLaserBeam_90angle_YEqualsTo_minus150(){
-		LaserBeam laserBeam = new LaserBeam(0.0D, 0.0D, 90.0D, 0L, 5L);
+	@Test(dataProvider = "angleSourceForY")
+	public void testLaserBeam_shouldCreateLaserBeam_YCoordinate(double x, double y, double angle, double expectedYCoordinate){
+		LaserBeam laserBeam = new LaserBeam(x, y, angle, 0L, 5L);
 		
 		Coordinate endPoint = laserBeam.getEndPoint();
 		
-		Assert.assertEquals(endPoint.getY(), 150.0D, 0.00001D);
+		Assert.assertEquals(endPoint.getY(), expectedYCoordinate, 0.00001D);
 	}
-	
-	@Test
-	public void testLaserBeam_shouldCreateLaserBeam_0Angle_YEqualsTo_0(){
-		LaserBeam laserBeam = new LaserBeam(0.0D, 0.0D, 0.0D, 0L, 5L);
-		
-		Coordinate endPoint = laserBeam.getEndPoint();
-		
-		Assert.assertEquals(endPoint.getY(), 0.0D, 0.00001D);
-	}
-	
-	@Test
-	public void testLaserBeam_shouldCreateLaserBeam_0Angle_XEqualsTo_150(){
-		LaserBeam laserBeam = new LaserBeam(0.0D, 0.0D, 0.0D, 0L, 5L);
-		
-		Coordinate endPoint = laserBeam.getEndPoint();
-		
-		Assert.assertEquals(endPoint.getX(), 150.0D, 0.00001D);
-	}
-	
 }
