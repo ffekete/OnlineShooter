183c1b4e0e386092f33fe53da107e7753c280cfd
Ferenc Fekete <Ferenc_Fekete@epam.com>
2016-08-16 12:28:45 +0200

    Added ConnectionNode tests.

--- src/main/java/game/connection/ConnectionPool.java
+++ src/main/java/game/connection/ConnectionPool.java
@@ -1,6 +1,5 @@
 package game.connection;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,11 +19,18 @@ public class ConnectionPool {
     private Map<Long, ConnectionNode> connectionPool;
             
     public void removeConnectionNode(Long playerId){
+        if(playerId == null){
+            throw new NullPointerException("(E): Player id cannot be null!");
+        }
         System.out.println("Connection node removed with id " + connectionPool.get(playerId).getId());
         connectionPool.remove(playerId);
     }
     
     public Long registerNewConnection(Long playerId){
+        if(playerId == null){
+            throw new NullPointerException("(E): Player id cannot be null!");
+        }
+        
         Long connectionId = findAvailableConnectionNodeId();
         
         /* If there is a free connection slot, register a new connection and store it in the pool */
@@ -46,27 +52,23 @@ public class ConnectionPool {
     }
     
     public Long findAvailableConnectionNodeId(){
-        if(this.isFull()){
+        if(this.isConnectionPoolFull()){
             return null;
         }
         else
         {
             /* Get all available connections by removing all used from the list */
-            ArrayList<Long> availableConnections = new ArrayList<Long>();
             for(long i = 0; i < ConnectionPreferences.SERVER_MAX_CAPACITY; i++){
-                availableConnections.add(i);
+                if(connectionPool.get(i) == null)
+                {
+                    return i;
+                }
             }
-            /* Remove used connection ids */
-            for(Long id: connectionPool.keySet()){
-                ConnectionNode node = connectionPool.get(id);
-                availableConnections.remove(node.getId());
-            }
-            
-            return availableConnections.get(0); // retrieve first element
         }
+        return null;        
     }
     
-    public boolean isFull(){
+    public boolean isConnectionPoolFull(){
         if(connectionPool.size() >= ConnectionPreferences.SERVER_MAX_CAPACITY){
             return true;
         }
---
+++ src/main/test/connection/ConnectionNodeTest.java
@@ -0,0 +1,43 @@
+package connection;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import factory.ConnectionNodeBuilder;
+import game.connection.ConnectionNode;
+
+public class ConnectionNodeTest{
+
+    
+    @Test
+    void testShouldCreateOneConnectionNode(){
+        ConnectionNode cn = new ConnectionNode("/TestPath_", 112L);
+        
+        Assert.assertEquals(cn.getPath(), "/TestPath_112");
+    }
+    
+    @Test
+    void testShouldCreateOneConnectionNodeWithBuilderClass(){
+        ConnectionNode cn = new ConnectionNodeBuilder().setConnectionId(Long.MAX_VALUE).setProvidePlayerDataPath("/TestPath__").build();
+        
+        Assert.assertEquals(cn.getPath(), "/TestPath__"+Long.MAX_VALUE);
+    }
+    
+    @DataProvider(name="invalidInputForConnectionNodeBuilder")
+    Object[][] invalidInputForConnectionNodeBuilder(){
+        return new Object[][]{
+            {null, 12L},
+            {"TestPath_", null},
+            {null, null}
+        };
+    }
+    
+    @Test(dataProvider="invalidInputForConnectionNodeBuilder",
+            expectedExceptions = {NullPointerException.class})
+    void testShouldThrowNullPointerException(String path, Long id){
+        new ConnectionNodeBuilder().setConnectionId(id).setProvidePlayerDataPath(path).build();
+        
+        
+    }
+}
