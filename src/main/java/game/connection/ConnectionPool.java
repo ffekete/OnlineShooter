d9b5a969762088e6aeb9e6d024b328a3c1c9996f
Tamas Schuszter <tamas_schuszter@epam.com>
2016-08-16 15:12:46 +0200

    Optional playerId test

--- src/main/java/game/connection/ConnectionPool.java
+++ src/main/java/game/connection/ConnectionPool.java
@@ -3,6 +3,7 @@ package game.connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -13,74 +14,76 @@ import game.config.ConnectionPreferences;
 
 @Component
 public class ConnectionPool {
-    
+
     @Autowired
     private ConnectionNodeBuilder connectionNodeBuilder;
-    
+
     private Map<Long, ConnectionNode> connectionPool;
-            
-    public void removeConnectionNode(Long playerId){
-        if(playerId == null){
-            throw new NullPointerException("(E): Player id cannot be null!");
-        }
-        System.out.println("Connection node removed with id " + connectionPool.get(playerId).getId());
-        connectionPool.remove(playerId);
+
+    public void removeConnectionNode(Long playerId) {
+        /*
+         * if(playerId == null){ throw new
+         * NullPointerException("(E): Player id cannot be null!"); }
+         */
+        Optional<Long> id = Optional.of(playerId);
+        System.out.println("Connection node removed with id " + playerId);
+        connectionPool.remove(id.get());
     }
-    
-    public Long registerNewConnection(Long playerId){
-        if(playerId == null){
+
+    public Long registerNewConnection(Long playerId) {
+        if (playerId == null) {
             throw new NullPointerException("(E): Player id cannot be null!");
         }
-        
+
         Long connectionId = findAvailableConnectionNodeId();
-        
-        /* If there is a free connection slot, register a new connection and store it in the pool */
-        if(connectionId != null){
-            ConnectionNode connectionNode = connectionNodeBuilder.
-                                                setConnectionId(connectionId).
-                                                setProvidePlayerDataPath(BrokerPaths.PROVIDE_PLAYER_DATA).
-                                                build();
-            
+
+        /*
+         * If there is a free connection slot, register a new connection and
+         * store it in the pool
+         */
+        if (connectionId != null) {
+            ConnectionNode connectionNode = connectionNodeBuilder.setConnectionId(connectionId)
+                    .setProvidePlayerDataPath(BrokerPaths.PROVIDE_PLAYER_DATA).build();
+
             connectionPool.put(playerId, connectionNode);
-            System.out.println("Connection registered for player id " + playerId + " to path " + connectionNode.getPath());
+            System.out.println(
+                    "Connection registered for player id " + playerId + " to path " + connectionNode.getPath());
             return connectionId;
-        }
-        else
-        {
+        } else {
             /* There is no free connection slot... */
             return null;
         }
     }
-    
-    public Long findAvailableConnectionNodeId(){
-        if(this.isConnectionPoolFull()){
+
+    public Long findAvailableConnectionNodeId() {
+        if (this.isConnectionPoolFull()) {
             return null;
-        }
-        else
-        {
-            /* Get all available connections by removing all used from the list */
+        } else {
+            /*
+             * Get all available connections by removing all used from the list
+             */
             ArrayList<Long> availableConnections = new ArrayList<Long>();
-            for(long i = 0; i < ConnectionPreferences.SERVER_MAX_CAPACITY; i++){
+            for (long i = 0; i < ConnectionPreferences.SERVER_MAX_CAPACITY; i++) {
                 availableConnections.add(i);
             }
             /* Remove used connection ids */
-            for(Long id: connectionPool.keySet()){
+            for (Long id : connectionPool.keySet()) {
                 ConnectionNode node = connectionPool.get(id);
                 availableConnections.remove(node.getId());
             }
-            
+
             return availableConnections.get(0); // retrieve first element
         }
     }
-    
-    public boolean isConnectionPoolFull(){
-        if(connectionPool.size() >= ConnectionPreferences.SERVER_MAX_CAPACITY){
+
+    public boolean isConnectionPoolFull() {
+        if (connectionPool.size() >= ConnectionPreferences.SERVER_MAX_CAPACITY) {
             return true;
         }
         return false;
     }
-    
-    public ConnectionPool(){
+
+    public ConnectionPool() {
         connectionPool = new HashMap<Long, ConnectionNode>();
     }
 }
