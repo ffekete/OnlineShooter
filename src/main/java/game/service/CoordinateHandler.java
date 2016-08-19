1c5186c47e0f37adfb81c94108ab63624f6694af
Tamas Schuszter <schusztertamas@gmail.com>
2016-08-19 13:52:13 +0200

    resultX and resultY changed to Point2D object

--- src/main/java/game/service/CoordinateHandler.java
+++ src/main/java/game/service/CoordinateHandler.java
@@ -11,24 +11,30 @@ import game.interfaces.Spawnable;
 public class CoordinateHandler {
 
     public Point2D calculateItemCoordinates(Spawnable item, double speed) {
-        double resultX;
-        double resultY;
-
         double angle = item.getAngle() * Math.PI / 180.0d;
+        Point2D coordinate = new Point2D.Double((double) item.getX() + speed * Math.cos(angle),
+                (double) item.getY() + speed * Math.sin(angle));
 
-        resultX = (double) item.getX() + speed * Math.cos(angle);
-        resultY = (double) item.getY() + speed * Math.sin(angle);
+        if (coordinate.getX() > GameConfig.STAGE_POS_LIMIT_X) {
+            coordinate.setLocation(GameConfig.STAGE_NEG_LIMIT_X + (coordinate.getX() - GameConfig.STAGE_POS_LIMIT_X),
+                    coordinate.getY());
+        }
 
-        if (resultX > GameConfig.STAGE_POS_LIMIT_X)
-            resultX = GameConfig.STAGE_NEG_LIMIT_X + (resultX - GameConfig.STAGE_POS_LIMIT_X);
-        if (resultX < GameConfig.STAGE_NEG_LIMIT_X)
-            resultX = GameConfig.STAGE_POS_LIMIT_X + (resultX + GameConfig.STAGE_POS_LIMIT_X);
+        if (coordinate.getX() < GameConfig.STAGE_NEG_LIMIT_X) {
+            coordinate.setLocation(GameConfig.STAGE_POS_LIMIT_X + (coordinate.getX() + GameConfig.STAGE_POS_LIMIT_X),
+                    coordinate.getY());
+        }
 
-        if (resultY > GameConfig.STAGE_POS_LIMIT_Y)
-            resultY = GameConfig.STAGE_NEG_LIMIT_Y + (resultY - GameConfig.STAGE_POS_LIMIT_Y);
-        if (resultY < GameConfig.STAGE_NEG_LIMIT_Y)
-            resultY = GameConfig.STAGE_POS_LIMIT_Y + (resultY + GameConfig.STAGE_NEG_LIMIT_Y);
+        if (coordinate.getY() > GameConfig.STAGE_POS_LIMIT_Y) {
+            coordinate.setLocation(coordinate.getX(),
+                    GameConfig.STAGE_NEG_LIMIT_Y + (coordinate.getY() - GameConfig.STAGE_POS_LIMIT_Y));
+        }
 
-        return new Point2D.Double(resultX, resultY);
+        if (coordinate.getY() < GameConfig.STAGE_NEG_LIMIT_Y) {
+            coordinate.setLocation(coordinate.getX(),
+                    GameConfig.STAGE_POS_LIMIT_Y + (coordinate.getY() + GameConfig.STAGE_NEG_LIMIT_Y));
+        }
+
+        return coordinate;
     }
 }
