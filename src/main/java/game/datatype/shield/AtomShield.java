7b0b5381802186bb2678e14415bd096b9327b831
Tamás Schuszter <tamas_schuszter@epam.com>
2016-08-23 16:58:16 +0200

    Bugfix (Ejnye Feri) :D

--- src/main/java/game/datatype/shield/AtomShield.java
+++ src/main/java/game/datatype/shield/AtomShield.java
@@ -1,9 +1,11 @@
 package game.datatype.shield;
 
 import game.config.constant.ShieldConfig;
+import game.service.Spawner;
 
-public class AtomShield extends ShieldParent{
-    public AtomShield(){
+public class AtomShield extends ShieldParent {
+    public AtomShield() {
+        Spawner.spawn(this);
         setName("Atom shield");
         setMaxProtectionValue(ShieldConfig.ATOM_SHIELD_PROTECTION);
         setProtection(getMaxProtectionValue());
--- src/main/java/game/datatype/shield/PlasmaShield.java
+++ src/main/java/game/datatype/shield/PlasmaShield.java
@@ -1,9 +1,11 @@
 package game.datatype.shield;
 
 import game.config.constant.ShieldConfig;
+import game.service.Spawner;
 
-public class PlasmaShield extends ShieldParent{
-    public PlasmaShield(){
+public class PlasmaShield extends ShieldParent {
+    public PlasmaShield() {
+        Spawner.spawn(this);
         setName("Plasma shield");
         setMaxProtectionValue(ShieldConfig.PLASMA_SHIELD_PROTECTION);
         setProtection(getMaxProtectionValue());
