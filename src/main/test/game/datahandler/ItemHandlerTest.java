5b2008c6147f81c4f8e729a2e840f9643d24e7df
Tamás Schuszter <tamas_schuszter@epam.com>
2016-08-23 15:20:20 +0200

    Test for itemHandler item drop

---
+++ src/main/test/game/datahandler/ItemHandlerTest.java
@@ -0,0 +1,67 @@
+package game.datahandler;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import game.config.constant.GameConfig;
+import game.entrypoint.Application;
+import game.interfaces.SpawnableItem;
+
+@ContextConfiguration(classes = { Application.class })
+public class ItemHandlerTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    ItemPool itemPool;
+
+    @Autowired
+    ItemHandler itemHandler;
+
+    @BeforeMethod
+    public void initPool() {
+        itemPool.clear();
+    }
+
+    @Test
+    public void shouldDropOneSpawnableitem() {
+        List<SpawnableItem> carriage = new ArrayList<SpawnableItem>();
+        carriage.add(new ItemCreationHandler().createRandomItem());
+
+        itemHandler.dropCargoToCoordinate(carriage, new Point2D.Double(0, 0));
+
+        Assert.assertEquals(itemPool.poolSize(), 1);
+    }
+
+    @Test(dataProvider = "coordinatesData")
+    public void droppedItemCoordinateShouldBetweenRange(double x, double y) {
+        List<SpawnableItem> carriage = new ArrayList<SpawnableItem>();
+        carriage.add(new ItemCreationHandler().createRandomItem());
+
+        itemHandler.dropCargoToCoordinate(carriage, new Point2D.Double(x, y));
+
+        Iterator<SpawnableItem> bit = itemPool.getIterator();
+        while (bit.hasNext()) {
+            SpawnableItem droppedItem = bit.next();
+
+            Assert.assertTrue((droppedItem.getX() > (-1 * GameConfig.MAX_ITEM_DROP_DISTANCE) + x)
+                    && (droppedItem.getX() < GameConfig.MAX_ITEM_DROP_DISTANCE + x)
+                    && (droppedItem.getY() > (-1 * GameConfig.MAX_ITEM_DROP_DISTANCE) + y)
+                    && (droppedItem.getY() < GameConfig.MAX_ITEM_DROP_DISTANCE + y));
+        }
+    }
+
+    @DataProvider(name = "coordinatesData")
+    public Object[][] getDozensData() {
+        return new Object[][] { { 1, 1 }, { 10, 10 }, { 15.6, 12.3 }, { 89.91, 87.2 }, { -1, -1 }, { -10, -10 },
+                { -100, -100 }, { -1000, -1000 } };
+    }
+}
